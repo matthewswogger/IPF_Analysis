aa1ca533e6771d6197d46f43a61e051516897685
Matthew Swogger <matthewswogger@yahoo.com>
2016-11-14 20:23:35 -0800

    plotly is acting weird

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 Looking at the differences between men and women in the IPF.
 It looks like there is a difference.
 
+## Visualization Using Bokeh
 To run enter: `bokeh serve --show ipf_graphs.py` into the command line within
 the directory that `ipf_graphs.py` is located.
+
+## Visualization Using Plotly
+To view my plotly graph go to https://plot.ly/~matthewswogger/4.embed
--- plotly_ipf.py
+++ plotly_ipf.py
@@ -36,178 +36,129 @@ df_women.loc[:,('average_deadlift')] = womens_array[:,2]
 
 df = pd.concat([df_men,df_women])
 #########################################################
+# a few variables assigned for readability and ease of changing them
+dot_size = 8
+line_width = 1
+line_color = 'rgb(0, 0, 0)'
+men_color = 'rgba(152, 0, 0, .8)'
+women_color = 'rgba(25, 219, 128, .9)'
 
 # men and women best squats and benches
 men_best_squat_bench = Scatter(x=df.best_squat[df.sex == 'M'],
                                y=df.best_bench[df.sex == 'M'],
                                mode = 'markers',
-                               marker = dict(size = 10,color = 'rgba(152, 0, 0, .8)',
-                                             line = dict(width = 2,color = 'rgb(0, 0, 0)')),
-                               name='Men')
+                               marker = dict(size=dot_size,color=men_color,
+                                             line=dict(width=line_width,color=line_color)),
+                               name = 'Men')
 
 women_best_squat_bench = Scatter(x=df.best_squat[df.sex == 'W'],
                                  y=df.best_bench[df.sex == 'W'],
                                  mode = 'markers',
-                                 marker = dict(size = 10,color = 'rgba(255, 182, 193, .9)',
-                                               line = dict(width = 2)),
-                                 name='Women',
-                                 visible=False)
+                                 marker = dict(size=dot_size,color=women_color,
+                                               line=dict(width=line_width)),
+                                 name = 'Women')
 
 # men and women best deadlifts and benches
 men_best_dead_bench = Scatter(x=df.best_deadlift[df.sex == 'M'],
                               y=df.best_bench[df.sex == 'M'],
                               mode = 'markers',
-                              marker = dict(size = 10,color = 'rgba(152, 0, 0, .8)',
-                                            line = dict(width = 2,color = 'rgb(0, 0, 0)')),
-                              name='Men',
-                              visible=False)
+                              marker = dict(size=dot_size,color=men_color,
+                                            line=dict(width=line_width,color=line_color)),
+                              name = 'Men',
+                              visible = False)
 
 women_best_dead_bench = Scatter(x=df.best_deadlift[df.sex == 'W'],
                                 y=df.best_bench[df.sex == 'W'],
                                 mode = 'markers',
-                                marker = dict(size = 10,color = 'rgba(255, 182, 193, .9)',
-                                              line = dict(width = 2)),
-                                name='Women',
-                                visible=False)
+                                marker = dict(size=dot_size,color=women_color,
+                                              line=dict(width=line_width)),
+                                name = 'Women',
+                                visible = False)
 
 # men and women squat class and bench squat ratio
 men_squat_class_ratio = Scatter(x=df.squat_class[df.sex == 'M'],
                                 y=df.bench_squat_ratio[df.sex == 'M'],
                                 mode = 'markers',
-                                marker = dict(size = 10,color = 'rgba(152, 0, 0, .8)',
-                                              line = dict(width = 2,color = 'rgb(0, 0, 0)')),
-                                name='Men',
-                                visible=False)
+                                marker = dict(size=dot_size,color=men_color,
+                                              line=dict(width=line_width,color=line_color)),
+                                name = 'Men',
+                                visible = False)
 
 women_squat_class_ratio = Scatter(x=df.squat_class[df.sex == 'W'],
                                   y=df.bench_squat_ratio[df.sex == 'W'],
                                   mode = 'markers',
-                                  marker = dict(size = 10,color = 'rgba(255, 182, 193, .9)',
-                                                line = dict(width = 2)),
-                                  name='Women',
-                                  visible=False)
+                                  marker = dict(size=dot_size,color=women_color,
+                                                line=dict(width=line_width)),
+                                  name = 'Women',
+                                  visible = False)
 
 # men and women average squat and bench by squat class
 men_average_squat_bench = Scatter(x=df.average_squat[df.sex == 'M'],
                                   y=df.average_bench[df.sex == 'M'],
                                   mode = 'markers',
-                                  marker = dict(size = 10,color = 'rgba(152, 0, 0, .8)',
-                                                line = dict(width = 2,color = 'rgb(0, 0, 0)')),
-                                  name='Men',
-                                  visible=False)
+                                  marker = dict(size=dot_size,color=men_color,
+                                                line=dict(width=line_width,color=line_color)),
+                                  name = 'Men',
+                                  visible = False)
 
 women_average_squat_bench = Scatter(x=df.average_squat[df.sex == 'W'],
                                     y=df.average_bench[df.sex == 'W'],
                                     mode = 'markers',
-                                    marker = dict(size = 10,color = 'rgba(255, 182, 193, .9)',
-                                                  line = dict(width = 2)),
-                                    name='Women',
-                                    visible=False)
+                                    marker = dict(size=dot_size,color=women_color,
+                                                  line=dict(width=line_width)),
+                                    name = 'Women',
+                                    visible = False)
 
 # men and women average deadlift and bench by squat class
 men_average_deadlift_bench = Scatter(x=df.average_deadlift[df.sex == 'M'],
                                      y=df.average_bench[df.sex == 'M'],
                                      mode = 'markers',
-                                     marker = dict(size = 10,color = 'rgba(152, 0, 0, .8)',
-                                                   line = dict(width = 2,color = 'rgb(0, 0, 0)')),
-                                     name='Men',
-                                     visible=False)
+                                     marker = dict(size=dot_size,color=men_color,
+                                                   line=dict(width=line_width,color=line_color)),
+                                     name = 'Men',
+                                     visible = False)
 
 women_average_deadlift_bench = Scatter(x=df.average_deadlift[df.sex == 'W'],
                                        y=df.average_bench[df.sex == 'W'],
                                        mode = 'markers',
-                                       marker = dict(size = 10,color = 'rgba(255, 182, 193, .9)',
-                                                     line = dict(width = 2)),
-                                       name='Women',
-                                       visible=False)
+                                       marker = dict(size=dot_size,color=women_color,
+                                                     line=dict(width=line_width)),
+                                       name = 'Women',
+                                       visible = False)
 
 ####################################################
 layout = Layout(title='IPF Data Analysis',
                 xaxis={'title': 'Squat or Deadlift'},
                 yaxis={'title': 'Bench or Bench/Squat Ratio'},
                 updatemenus=list([dict(yanchor='top',
-                                       buttons=list([# best bench on yaxis, best squat on xaxis
-                                                     dict(args=['visible', [True,False,False,False,
-                                                                            False,False,False,False,
-                                                                            False,False]],
-                                                          label='Men BestSquat BestBench',
-                                                          method='restyle'),
+                                       buttons=list([
                                                      dict(args=['visible', [True,True,False,False,
                                                                             False,False,False,False,
                                                                             False,False]],
-                                                          label='Men Women BestSquat BestBench',
-                                                          method='restyle'),
-                                                     dict(args=['visible', [False,True,False,False,
-                                                                            False,False,False,False,
-                                                                            False,False]],
-                                                          label='Women BestSquat BestBench',
-                                                          method='restyle'),
-                                                     # best bench on yaxis, best deadlift on xaxis
-                                                     dict(args=['visible', [False,False,True,False,
-                                                                            False,False,False,False,
-                                                                            False,False]],
-                                                          label='Men BestDead BestBench',
+                                                          label='X:BestSquat Y:BestBench',
                                                           method='restyle'),
                                                      dict(args=['visible', [False,False,True,True,
                                                                             False,False,False,False,
                                                                             False,False]],
-                                                          label='Men Women BestDead BestBench',
-                                                          method='restyle'),
-                                                     dict(args=['visible', [False,False,False,True,
-                                                                            False,False,False,False,
-                                                                            False,False]],
-                                                          label='Women BestDead BestBench',
-                                                          method='restyle'),
-                                                     # squat class on xaxis, bench/squat ratio on yaxis
-                                                     dict(args=['visible', [False,False,False,False,
-                                                                            True,False,False,False,
-                                                                            False,False]],
-                                                          label='Men SquatClass Bench/Squat',
+                                                          label='X:BestDead Y:BestBench',
                                                           method='restyle'),
                                                      dict(args=['visible', [False,False,False,False,
                                                                             True,True,False,False,
                                                                             False,False]],
-                                                          label='Men Women SquatClass Bench/Squat',
-                                                          method='restyle'),
-                                                     dict(args=['visible', [False,False,False,False,
-                                                                            False,True,False,False,
-                                                                            False,False]],
-                                                          label='Women SquatClass Bench/Squat',
-                                                          method='restyle'),
-                                                     # average squat on xaxis, average bench on yaxis
-                                                     dict(args=['visible', [False,False,False,False,
-                                                                            False,False,True,False,
-                                                                            False,False]],
-                                                          label='Men Average Bench Squat',
+                                                          label='X:SquatClass Y:Bench/Squat',
                                                           method='restyle'),
                                                      dict(args=['visible', [False,False,False,False,
                                                                             False,False,True,True,
                                                                             False,False]],
-                                                          label='Men Women Average Bench Squat',
-                                                          method='restyle'),
-                                                     dict(args=['visible', [False,False,False,False,
-                                                                            False,False,False,True,
-                                                                            False,False]],
-                                                          label='Women Average Bench Squat',
-                                                          method='restyle'),
-                                                     # average deadlift on xaxis, average bench on yaxis
-                                                     dict(args=['visible', [False,False,False,False,
-                                                                            False,False,False,False,
-                                                                            True,False]],
-                                                          label='Men Average Bench Deadlift',
+                                                          label='Average X:Squat Y:Bench',
                                                           method='restyle'),
                                                      dict(args=['visible', [False,False,False,False,
                                                                             False,False,False,False,
                                                                             True,True]],
-                                                          label='Men Women Average Bench Deadlift',
-                                                          method='restyle'),
-                                                     dict(args=['visible', [False,False,False,False,
-                                                                            False,False,False,False,
-                                                                            False,True]],
-                                                          label='Women Average Bench Deadlift',
+                                                          label='Average X:Deadlift Y:Bench',
                                                           method='restyle')
                                                     ])
-                                                    )]),)
+                                                    )]))
 ##########################################################
 data = Data([men_best_squat_bench,
              women_best_squat_bench,
